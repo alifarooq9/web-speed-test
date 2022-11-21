7da38f0af772dfc70336401132324db5ce57a888
Ali Muhammad <alimuhammadfc5@gmail.com>
2022-11-22 04:28:57 +0500

    is it fixed?

--- pages/api/getEdge.ts
+++ pages/api/getEdge.ts
@@ -6,18 +6,10 @@ export const config = {
 };
 
 export default async function handler(req: NextRequest) {
-	await redis.hget("greeting", "869dfe15-7dee-457f-a894-39416fab33e4");
-	// console.log(greetings);
-
-	return new Response(
-		JSON.stringify({
-			greetings: "hello",
-		}),
-		{
-			status: 200,
-			headers: {
-				"content-type": "application/json",
-			},
-		}
+	const greetings = await redis.hget(
+		"greeting",
+		"869dfe15-7dee-457f-a894-39416fab33e4"
 	);
+	console.log(greetings);
+	return new Response(JSON.stringify("Hello"));
 }
