b6dc58cb9b361d7dd12625f321a981f201be14e1
Ali Muhammad <alimuhammadfc5@gmail.com>
2022-11-22 04:25:03 +0500

    checking if edge database is working>

--- pages/api/getEdge.ts
+++ pages/api/getEdge.ts
@@ -6,17 +6,12 @@ export const config = {
 };
 
 export default async function handler(req: NextRequest) {
-	const greetings = await redis.hget(
-		"greeting",
-		"869dfe15-7dee-457f-a894-39416fab33e4"
-	);
-	console.log(greetings);
+	await redis.hget("greeting", "869dfe15-7dee-457f-a894-39416fab33e4");
+	// console.log(greetings);
 
 	return new Response(
 		JSON.stringify({
-			// id: JSON.parse(greetings[0]).id,
-			// greetings: JSON.parse(greetings[0]).greetings + " " + "from edge",
-			id: "hello",
+			greetings: "hello",
 		}),
 		{
 			status: 200,
